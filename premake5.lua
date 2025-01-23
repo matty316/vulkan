4e4c7ad1424233ddf54fe4ae4780d471e3bf2f3d
matty <mattreed1890@gmail.com>
2025-01-23 17:23:53 -0600

    show window

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,21 @@
-workspace "WorkspaceName"
+workspace "VulkanProj"
     configurations { "Debug", "Release" }
     platforms { "Win64" }
 
-project "ProjectName"
+project "VulkanProj"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
     cppdialect "C++23"
 
-    files { "include/**.h", "src/**.c", "src/**.cpp" }
+    files { "include/**.h", "inlcude/**.hpp", "src/**.c", "src/**.cpp" }
 
     includedirs { "include" }
     includedirs { "src" }
+    libdirs { "lib" }
+
+    links { "glfw3" }
+    links { "vulkan-1" }
 
     filter "configurations:Debug"
     defines { "DEBUG" }
