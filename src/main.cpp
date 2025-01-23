ef84bac1bdf0d7214188484e234e0871774f0bfd
matty <mattreed1890@gmail.com>
2025-01-23 17:28:30 -0600

    typo

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ private:
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-		window = glfwCreateWindow(-WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
 	void initVulkan() {
